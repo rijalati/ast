6c4d19d15b8f8aa24b74f45d0b9bc5c6c3002d02
Ritchie J Latimore <ritchie.latimore@bluemedora.com>
2017-09-28 01:26:35 +0000

    inititial commit

---
+++ a-custom-builder.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export CC="/usr/bin/gcc-4.9 -lm -ldl -std=gnu99"
+export AR="/usr/bin/ar"
+TMPFILE="$(mktemp 00-build-out.XXXXXX)"
+export TMPFILE
+export CCFLAGS='-D_PACKAGE_ast=0 -DSHOPT_SYSRC -D_map_libc=1 -D_BLD_ast=1 -DSHOPT_CMDLIB_BLTIN=1 -D_BLD_cmd=1 -Dvt_threaded=1 -D_hdr_fcntl=1 -D_BLD_DLL=1 -D_BLD_sfio=1 -D_BLD_stdio=1 -D_astimport=1 -D_DLL=1 -D_dll_import=1 -D_lib_unlink=1 -D_hdr_mmap=1 -D_hdr_stdlib=1 -D_sys_types=1 -DAT_FDCWD=1 -DSH_CMDLIB_DIR="/usr/ast/bin" -DSHOPT_CMDLIB_HDR="tmp_gnulinux_builtin_header.h" -D_GNU_SOURCE=1 -D__USE_GNU=1'
+#bin/package never only make ast-ast PACKAGE_OPTIONS='map-libc' CCFLAGS="${CCFLAGS}" HOSTTYPE="linux.i386-64" SHELL='/bin/sh' COSHELL="/bin/sh" | tee "${TMPFILE}"
+#eval /opt/ast/bin/ksh bin/package verbose only make ast-ast PACKAGE_OPTIONS='map-libc' CCFLAGS="${CCFLAGS}" HOSTTYPE="linux.i386-64" SHELL='/bin/sh' | tee -a "${TMPFILE}"
+eval /opt/ast/bin/ksh bin/package verbose only make ast-base PACKAGE_OPTIONS='map-libc' CCFLAGS="${CCFLAGS}" HOSTTYPE="linux.i386-64" SHELL='/bin/sh' | tee -a "${TMPFILE}"
+#EOF.
